9a61bb82516e6d415d04c62d5cbf27304eff482d
WangLuyi <10195101513@stu.ecnu.edu.cn>
2023-03-20 21:34:57 +0800

    全景导览图test

---
+++ src/components/Vtour.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Vtour"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,41 @@
 <template>
-  <div>
-    <el-row>
-      <el-col :span=24>
-        <el-card class="card">
-          <h2>虚拟宠物医院学习系统 v1.0</h2>
-        </el-card>
-      </el-col>
-    </el-row>
+  <!--  <div>-->
+  <!--    <el-row>-->
+  <!--      <el-col :span=24>-->
+  <!--        <el-card class="card">-->
+  <!--          <h2>虚拟宠物医院学习系统 v1.0</h2>-->
+  <!--        </el-card>-->
+  <!--      </el-col>-->
+  <!--    </el-row>-->
+  <!--  </div>-->
+
+  <div id="pano" style="width:100%;height:100%;">
+    <noscript>
+      <table style="width:100%;height:100%;">
+        <tr style="vertical-align:middle;">
+          <td>
+            <div style="text-align:center;">
+              ERROR:
+              <br/>
+              <br/>Javascript not activated
+              <br/>
+              <br/>
+            </div>
+          </td>
+        </tr>
+      </table>
+    </noscript>
   </div>
 </template>
 
 <script>
+var krpano = null;
+
+function krpano_onready_callback(krpano_interface) {
+  krpano = krpano_interface;
+}
+
+
 export default {
   name: 'Home',
   data() {
@@ -18,9 +43,28 @@ export default {
       items: []
     }
   },
-  methods: {},
-  mounted() {
-  }
+  methods: {
+    async getHotspots(currentPage, pageSize) {
+      const dataTest = await getHotspot({
+        pageNum: currentPage,
+        pageSize: pageSize
+      });
+      //实现js和xml交互
+      krpano.call('set(hotspot[spot_0_2].title,' + dataTest.data.message + ')')
+    }
+  },
+  mounted: function () {
+    embedpano({
+      swf: "../../static/vtour/tour.swf",
+      xml: "../../static/vtour/tour.xml",
+      target: "pano",
+      html5: "auto",
+      mobilescale: 1.0,
+      passQueryParameters: true,
+      onready: krpano_onready_callback //回调函数，获取Krpano 对象
+    });
+    this.getHotspots(1, 3);
+  },
 }
 
 </script>
